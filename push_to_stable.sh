15041b47f87ddd8387052f07e8de34f64fe04808
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-06 15:18:21 +0100

    Git push 17

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -13,8 +13,10 @@
         cd repo
 		
    git checkout stable
-    git pull origin master
+  
 	git add .
 	git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to stable"
     git push > /dev/null
     echo -e "Pushed to GitHub"
+	
+	  git pull origin master
