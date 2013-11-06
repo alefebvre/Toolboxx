26621ba69dc32feca569ea6d7b7cc541997700aa
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-06 14:20:58 +0100

    Git push to stable 6

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -7,9 +7,8 @@
 
    git branch
    
-   git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git repo > /dev/null || error_exit "Error cloning the repository";
-     # go into repo anc copy data
-        cd repo
+
+
    git checkout stable
 	
-    git pull 
+    git pull origin master
