fd5cf78a7c1a83f946e40acf8d0d8d45ce197df5
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-06 15:14:31 +0100

    Git push 16

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
     git config --global user.email "aurelien.lefebvre@viacesi.fr"
 	git config --global user.name "alefebvre"
+	 git config --global push.default matching
 
        
 
    git branch
    
-   git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git  > /dev/null || error_exit "Error cloning the repository";
-     # go into repo anc copy data
-   
+        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git repo > /dev/null || error_exit "Error cloning the repository";
+
+        # go into repo anc copy data
+        cd repo
 		
    git checkout stable
     git pull origin master
