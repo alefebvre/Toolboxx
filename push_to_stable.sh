6b326c380a305500feeaaad0fec903fba85a9019
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-06 16:32:10 +0100

    Git push 2

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -3,7 +3,14 @@
 	git config --global user.name "alefebvre"
 	 git config --global push.default matching
 
+       
 
+   git branch
+   
+        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git repo > /dev/null || error_exit "Error cloning the repository";
+
+     # go into repo anc copy data
+    cd repo
 		
    git checkout stable
    git pull origin master
