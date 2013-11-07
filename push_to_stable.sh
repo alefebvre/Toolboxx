e44ad5bddc075e3a08342f455ee3ee135b0293f3
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-07 11:47:34 +0100

    Hey

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -17,14 +17,6 @@
  		
     git checkout stable
     git pull origin master
-<<<<<<< HEAD
-	git commit -m "travis stable"
-=======
 	git add *
-<<<<<<< HEAD
-	git commit -m "LOOOOOL"
->>>>>>> 3fa3f4d5b980cd2ed72ac6b0510529be3f9d47f6
-=======
 	git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to stable"
->>>>>>> f7e1af002789d6e26801de2395388e1531238da8
 	git push
