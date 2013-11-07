d316a9d37277b76b03ed6bab4b625532e5785d50
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-07 10:03:04 +0100

    Test

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
+
+
+	# copy data we're interested in to other place
+		cp -Rv Application $HOME/Application
+        cp -Rv test $HOME/test
+
+  # go to home and setup git
+	cd $HOME
     git config --global user.email "aurelien.lefebvre@viacesi.fr"
-	git config --global user.name "alefebvre"
+	git config --global user.name "Travis"
 	 git config --global push.default matching
 
+	  
 	 
 	 # using token clone gh-pages branch
-        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git  > /dev/null || error_exit "Error cloning the repository";
+        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Toolboxx.git repo > /dev/null || error_exit "Error cloning the repository";
 
-
+        # go into repo and copy data
+        cd repo
+	   # checkout stable
+	   git checkout -b stable
+	   
+	     # copy stuff
+	  cp -Rv $HOME/build/Application Application/
+       cp -Rv $HOME/build/test test/
 	 
-	 
-   git checkout stable
    
+       # Pull, add, commit and push files
    git pull origin master
 
+		git add .
+        git commit -m "Travis Stable"
+        git push 
