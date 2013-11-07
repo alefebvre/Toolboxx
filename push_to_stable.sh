8d02d9508dd08d23b272232047d3d458c2f31850
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-07 09:29:09 +0100

    Git Push to stable

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -12,11 +12,12 @@
      # go into repo anc copy data
     cd repo
 		
-   git checkout stable
+   git checkout -b stable
    git pull origin master
-        git add *
-        git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to stable"
-        git push > /dev/null
+   git status
+        git add .
+        git commit -m "Travis Stable"
+        git push 
         echo -e "Pushed to GitHub"
 	
 	 
